8d94977dcc705faae39c7fa05967899d52e99511
ramez <ramez.elzoz74@gmail.com>
2026-02-09 14:00:49 +0200

    fetching order by his id

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!searchQuery) return;
+    navigate(`/order/${searchQuery}`);
+    setSearchQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search orders #"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
