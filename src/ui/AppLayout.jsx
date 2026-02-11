785104943806bc4e5cce8722799225d428cae17b
ramez <ramez.elzoz74@gmail.com>
2026-02-11 20:59:28 +0200

    styling the loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,15 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import {
+  Outlet,
+  useNavigation,
+} from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation(); // For showing a loading state when navigating between routes. The useNavigation hook returns an object with information about the current navigation state, including whether a navigation is in progress (state === "loading").
-  const isLoading = navigation.state === "loading";
+  const isLoading =
+    navigation.state === "loading";
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
